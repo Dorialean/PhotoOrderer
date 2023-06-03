b6eba3671fee1e4d9e6e5b7b9a9f00d2e1519787
Dorialean <lana.lakutina@mail.ru>
2023-06-03 20:29:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿string workingDirectory = Environment.CurrentDirectory;
+string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+var photoDir = new DirectoryInfo(Path.Combine(projectDirectory, "photos"));
+var orderedDir = new DirectoryInfo(Path.Combine(projectDirectory, "ordered"));
+
+// Файл к его году
+Dictionary<FileInfo, int> fileToYear = new();
+
+FillFileToDate(photoDir);
+
+foreach (var ftd in fileToYear)
+{
+    var photo = ftd.Key;
+    var year = ftd.Value;
+    if (orderedDir.GetDirectories()
+		.Any(dir => dir.Name == ftd.Value.ToString()))
+	{
+		var path = Path.Combine(orderedDir.FullName, year.ToString());
+		try
+		{
+            photo.MoveTo(Path.Combine(path, photo.Name));
+        }
+		catch(IOException e)
+		{
+			continue;
+		}
+    }
+	else
+	{
+        var path = Path.Combine(orderedDir.FullName, year.ToString());
+		Directory.CreateDirectory(path);
+        try
+        {
+            photo.MoveTo(Path.Combine(path, photo.Name));
+        }
+        catch (IOException e)
+        {
+            continue;
+        }
+	}
+}
+Console.WriteLine(GetDirectorySize(orderedDir.FullName));
+
+void FillFileToDate(DirectoryInfo dir)
+{
+	if (dir.Exists)
+	{
+		FileInfo[] photos = dir.GetFiles();
+		foreach (var photo in photos)
+		{
+            fileToYear.Add(photo, photo.LastWriteTime.Year);
+        }
+	}
+}
+
+static long GetDirectorySize(string parentDirectory)
+{
+    return new DirectoryInfo(parentDirectory).GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length);
+}
+
+
+
+
+
